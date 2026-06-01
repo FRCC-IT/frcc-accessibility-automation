13bcdd656ab40ac88e2606e462258e265bb72976
mzml <hi@mzml.io>
2026-06-01 12:00:51 -0600

    Update Playwright configuration and restore Front Range Search test

--- playwright.config.js
+++ playwright.config.js
@@ -5,11 +5,21 @@ const config = {
   ...screenReaderConfig,
   reportSlowTests: null,
   timeout: 3 * 60 * 1000,
-  retries: 2,
+  retries: 0,
   projects: [
     {
       name: "webkit",
-      use: { ...devices["Desktop Safari"], headless: false },
+      use: {
+        ...devices["Desktop Safari"],
+        headless: false,
+        launchOptions: {
+          args: [
+            '--start-maximized',     // Works primarily on Windows/Linux
+            '--window-position=0,0', // Positions browser at the top-left coordinate of macOS
+            '--window-size=1920,1080' // Manually specifies the boundary width and height
+          ]
+        },
+      },
     },
   ],
 };
